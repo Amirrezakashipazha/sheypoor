47fd390ddd65ff0c4bccf4760d9608c568e1c0b2
Amirreza <amirrezakashipazha@gmail.com>
2022-07-25 10:22:52 +0430

    merg files

---
+++ javascript.js
@@ -0,0 +1,35 @@
+function scrollWinL() {
+    document.getElementById("demo").scrollBy(-200, 0);
+}
+
+function scrollWinR() {
+    document.getElementById("demo").scrollBy(200, 0);
+}
+
+
+
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+
+window.onclick = function (e) {
+    if (!e.target.matches('.dropbtn')) {
+        var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
